246985746ca7027d992161f72bf9cb69e127a9d5
Aleksandr <119818692+uukeyi@users.noreply.github.com>
2023-03-30 21:37:33 +0300

    copress images

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const words = ['5 ЛЕТ'];
+const words = ["5 ЛЕТ"];
 let part;
 let i = 0;
 let offset = 0;
@@ -9,37 +9,51 @@ const skip_delay = 15;
 const speed = 200;
 
 const wordflick = function () {
-  setInterval(function () {
-    if (forwards) {
-      if (offset >= words[i].length) {
-        ++skip_count;
-        if (skip_count == skip_delay) {
-          forwards = false;
-          skip_count = 0;
-        }
-      }
-    } else {
-      if (offset == 0) {
-        forwards = true;
-        i++;
-        offset = 0;
-        if (i >= len) {
-          i = 0;
-        }
-      }
-    }
-    part = words[i].substr(0, offset);
-    if (skip_count == 0) {
+   setInterval(function () {
       if (forwards) {
-        offset++;
+         if (offset >= words[i].length) {
+            ++skip_count;
+            if (skip_count == skip_delay) {
+               forwards = false;
+               skip_count = 0;
+            }
+         }
       } else {
-        offset--;
+         if (offset == 0) {
+            forwards = true;
+            i++;
+            offset = 0;
+            if (i >= len) {
+               i = 0;
+            }
+         }
       }
-    }
-    document.querySelector('.about-us__left-title').textContent = `${part}I`;
-  }, speed);
+      part = words[i].substr(0, offset);
+      if (skip_count == 0) {
+         if (forwards) {
+            offset++;
+         } else {
+            offset--;
+         }
+      }
+      document.querySelector(".about-us__left-title").textContent = `${part}I`;
+   }, speed);
 };
 
-window.addEventListener('DOMContentLoaded', function () {
-  wordflick();
+window.addEventListener("DOMContentLoaded", function () {
+   wordflick();
 });
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const blockID = anchor.getAttribute('href').substr(1);
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  });
+}
