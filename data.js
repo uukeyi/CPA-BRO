10796f0ba344c9136dc916fce5b45b38bfdf0908
Aleksandr <119818692+uukeyi@users.noreply.github.com>
2023-04-06 22:17:39 +0300

    make all site

---
+++ data.js
@@ -0,0 +1,216 @@
+const obj = {
+   ru: {
+      aboutUs: {
+         title: "О НАС",
+         yearsMainTitle: "5 ЛЕТ",
+         yearsMainSubtitle: "На рынке CPA и iGaming",
+         developApplication:
+            "Собственная разработка Android и iOS приложений для CPA BRO, а также сервис для внешней аренды BroApps",
+         teamBlock: "Больше 4 ww in-house команд",
+         communityBlock: "Свое закрытое комьюнити FULL HOUSE",
+         meetupBlock: "Серия online Meet-up Aff2Traff",
+         cazinoWithOffers:
+            "3 sportsbook & casino продукта по лицензиям Curacao и MGA, Estonian",
+         creoBro: "Сервис креативов SHINOBI",
+         yearsSecondaryTitle: "3 ГОДА",
+         yearsSecondarySubtitle:
+            "«Лучшая Gambling & Betting партнерская программа» по версии Affbank Awards 20/21/22",
+         educationResource: "Собственный образовательный ресурс PressAff",
+         paymentSolution:
+            "Собственное платежное решение ChocoPay для платформ Fb, Google, Tiktok",
+      },
+      joinUs: {
+         text: `<p class = "marquee__subtitle_ru"> CPA BRO — международная партнерская <br/> программа с опытом в iGaming индустрии <br/> более 5 лет. Мы предлагаем лучшие условия <br/> на деле, а не на словах.</p>`,
+      },
+      form: {
+         webmaster: "Вебмастер",
+         advertiser: "Рекламодатель",
+         goBack: "Назад",
+         or: "или",
+      },
+
+      offers: {
+         leon: [
+            "ГЕО — Португалия",
+            "источник — FB APPS",
+            "ставка — от 60 USD",
+            "мин.деп — 10 EUR",
+         ],
+         tedbet: [
+            " ГЕО — Южная Корея",
+            "источник — FB APPS",
+            "ставка — от 160 USD",
+            "мин.деп — 5 USD",
+         ],
+         onewin: [
+            "ГЕО — Бразилия",
+            "источник — FB APP",
+            "ставка — от 20 USD",
+            "мин.деп — 30 BRL",
+         ],
+         brazino: [
+            "ГЕО — Австралия",
+            "источник — PPC",
+            "ставка — от 40 USD",
+            "мин.деп — 30 BRL",
+         ],
+      },
+
+      faq: {
+         whichVertical: {
+            title: "С какими вертикалями вы работаете?",
+            answer:
+               "CPA BRO узконаправленная партнерская сеть, мы работаем исключительно с Gambling и Betting вертикалями",
+         },
+         whichGeo: {
+            title: "С какими ГЕО вы работаете?",
+            answer: "Мы работаем со всеми странами от Tier-1 до Tier-3.",
+         },
+         whichTrafficSources: {
+            title: "Какие источники трафика являются для вас приоритетными?",
+            answer:
+               "Мы фокусируемся на HQ источниках, таких как SEO, PPC, ASO, ASA, Facebook и In-App.",
+         },
+         whichModelsTrafficPayment: {
+            title: "С какими моделями оплаты трафика вы работаете?",
+            answer: "CPA, RevShare и Hybrid.",
+         },
+         availabilityInHouse: {
+            title: "У вас есть In-House команды?",
+            answer:
+               "Да, конечно. Одно из наших преимуществ — это внутренний медиабаинг команды под различные, приоритетные для нас и рекламодателей направления.",
+         },
+         howToCopperate: {
+            title: "Как можно начать сотрудничать с вами?",
+            answer: `Все просто: заполните <a href = #wrapper class = "faq_reg_link anchor" >форму</a>  представленную на сайте , после чего с вами свяжется наш саппорт, чтобы обсудить дальнейшие этапы сотрудничества.`,
+         },
+         noExperience: {
+            title: "У меня нет опыта в gambling вертикали, могу ли я с вами поработать?",
+            answer:
+               "Будучи приватной партнерской сетью, мы концентрируемся на работе с уже опытными вебмастерами. Однако, если у вас богатый опыт в других вертикалях и есть все необходимые ресурсы для запуска, мы без проблем подберем вам оффер и сориентируем по подходам.",
+         },
+         noVerification: {
+            title: "Мой аккаунт не верифицируют, что делать?",
+            answer:
+               "Возможно вы не предоставили информацию о вашем опыте, источниках или не прикрепили статистику. Просим предоставить полную информацию, это сократит время верификации.",
+         },
+         availableOffers: {
+            title: "Как мне получить доступ к офферам?",
+            answer:
+               "Самые «вкусные» продукты находятся в приватном доступе. После того, как саппорт или личный менеджер определит интересующее вас ГЕО, вам распишут все условия, откроют офферы и выдадут ссылки для залива!",
+         },
+         howToWorkWithUs: {
+            title: "Как мне начать работать с вами?",
+            answer:
+               "Все просто: нужно зарегистрироваться в партнерской сети и дождаться подождать ответа от @cpabrosupport. И, кстати, у нас самый быстрый саппорт на рынке!",
+         },
+      },
+   },
+   en: {
+      aboutUs: {
+         title: "ABOUT US",
+         yearsMainTitle: "5 YEARS",
+         yearsMainSubtitle: "In CPA and iGaming",
+         developApplication:
+            "In-house Android and iOS app development for CPA BRO partners, and application rental service BroApps",
+         teamBlock: "4+ in-house teams worldwide ",
+         communityBlock: "Private community Full House",
+         meetupBlock: "online Meet-ups Aff2Traff ",
+         cazinoWithOffers:
+            "3 sportsbook & casino offers with Curacao and MGA licenses (Estonian)",
+         creoBro: "Ad creatives service SHINOBI",
+         yearsSecondaryTitle: "3 YEARS",
+         yearsSecondarySubtitle:
+            "Best Gambling & Betting Affiliate Network according to AffBank Awards 20/21/22",
+         educationResource: "Educational hub PressAff ",
+         paymentSolution:
+            "Payment solution ChocoPay for Facebook, Google, and TikTok Ads",
+      },
+      joinUs: {
+         text: `<p>CPA BRO — an international affiliate network with <br/>  5+ years of expertise in iGaming. We walk the talk <br/>  when providing you the best deals.</p>`,
+      },
+      form: {
+         webmaster: "Webmaster",
+         advertiser: "Advertiser",
+         goBack: "Go Back",
+         or: "or",
+      },
+      offers: {
+         leon: [
+            "GEO — Portugal",
+            "Source — FB APPS",
+            "CPA — от 60 USD",
+            "mindep — 10 EUR",
+         ],
+         tedbet: [
+            "GEO — South Korea",
+            "Source — FB APPS",
+            "CPA — от 160 USD",
+            "mindep — 5 USD",
+         ],
+         onewin: [
+            "GEO — Brazil",
+            "Source — FB APP",
+            "CPA — от 20 USD",
+            "mindep — 30 BRL",
+         ],
+         brazino: [
+            "GEO — Australia",
+            "Source — PPC",
+            "CPA — от 40 USD",
+            "mindep — 30 BRL",
+         ],
+      },
+      faq: {
+         whichVertical: {
+            title: "What niches do you work with?",
+            answer:
+               "CPA BRO exclusively works with the Gambling and Betting niches.",
+         },
+         whichGeo: {
+            title: "What GEOs do you work with?",
+            answer: "We work with all countries from Tier-1 to Tier-3.",
+         },
+         whichTrafficSources: {
+            title: "What traffic sources are a priority for you?",
+            answer:
+               "We focus on high-quality traffic sources such as SEO, PPC, ASO, ASA, Facebook, and In-App.",
+         },
+         whichModelsTrafficPayment: {
+            title: "What traffic payment models do you offer?",
+            answer: "CPA, RevShare и Hybrid.",
+         },
+         availabilityInHouse: {
+            title: "Do you have in-house teams?",
+            answer:
+               "Yes, of course. One of our key advantages is the internal media buying teams for different directions that are essential to us and the advertisers.",
+         },
+         howToCopperate: {
+            title: "How can I start working with you?",
+            answer:
+               `It's simple: fill out the <a href = #wrapper class = "faq_reg_link anchor" >form</a> on the website, after which our support team will contact you to discuss further steps of our cooperation.`,
+         },
+         noExperience: {
+            title: "I don't have experience in the gambling niche, can I work with you?",
+            answer:
+               "As a private affiliate network, we focus on working with experienced webmasters. However, if you have rich experience in other niches and have all the necessary resources to launch, we can easily find an offer for you and get you up to speed with the most current approaches.",
+         },
+         noVerification: {
+            title: "My account is not being verified, what should I do?",
+            answer:
+               "Perhaps you have not provided information about your experience, preferred traffic sources, or have not attached statistics. Please, provide us with the complete information, this will reduce verification time.",
+         },
+         availableOffers: {
+            title: "How can I get access to offers?",
+            answer:
+               "The most profitable products are available by approval only. After the support or personal manager determines your GEOs of interest, they will describe all the conditions to you, grant access to the offers, and provide links for starting a campaign!",
+         },
+         howToWorkWithUs: {
+            title: "How do I start working with you?",
+            answer:
+               "It's simple: you need to register with the affiliate network and wait for a response from @cpabrosupport. And, by the way, we have the fastest support service in the market!",
+         },
+      },
+   },
+};
+export default obj;
